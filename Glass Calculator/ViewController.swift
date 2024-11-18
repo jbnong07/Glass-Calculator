b4de8f864313e02eb5c5a8a76d6b403f184d33f7
jbnong07 <jbnong07@naver.com>
2024-11-18 16:09:52 +0900

    🐛 [Fix] build bug

---
+++ Glass Calculator/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Glass Calculator
+//
+//  Created by 박진홍 on 11/18/24.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
