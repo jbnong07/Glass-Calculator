9184e2c6671dbc7765e0bcc63ed0111eaeda8a7d
jbnong07 <jbnong07@naver.com>
2024-11-22 19:53:57 +0900

    🐛 [Fix] comma format for negative num

--- Glass Calculator/View/Display/DisplayLabel.swift
+++ Glass Calculator/View/Display/DisplayLabel.swift
@@ -32,10 +32,15 @@ extension DisplayLabel {
     
     private func formatNum(num: String) -> String {
         let splitNum = num.split(separator: ".")
-        let intPart: Substring.SubSequence = splitNum[0]
-        let decimalPart: Substring.SubSequence? = (splitNum.count > 1 ? splitNum[1] : nil)
+        let isNegative: Bool = num.contains("-")//음수 처리를 위한 체크
+        var intPart: Substring.SubSequence = splitNum[0]
+        let decimalPart: Substring.SubSequence? = (splitNum.count > 1 ? splitNum[1] : nil)//정수와 소수부분 체크
         var commaFormatString: String = ""
         var commaCount: Int = 0
+        
+        if isNegative {//음수라면 - 제거
+            intPart.removeFirst()
+        }
         
         for num in intPart.reversed() {
             if commaCount == 3 {
@@ -50,6 +55,10 @@ extension DisplayLabel {
             commaFormatString = String(commaFormatString.reversed() + "." + String(decimalPart))
         } else {
             commaFormatString = String(commaFormatString.reversed())
+        }
+        
+        if isNegative {
+            commaFormatString = "-" + commaFormatString
         }
         
         return commaFormatString
